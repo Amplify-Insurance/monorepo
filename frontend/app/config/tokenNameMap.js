0486a407546629a93b8dc4d4fd8f03fe34371b4d
TJ <58812386+TJ-2@users.noreply.github.com>
2025-06-19 09:16:11 +0100

    commit

--- frontend/app/config/tokenNameMap.js
+++ frontend/app/config/tokenNameMap.js
@@ -1,23 +1,5 @@
 import { IdCard } from "lucide-react";
 
-// export const TOKEN_NAME_MAP = {
-//   '0x4e65fe4dba92790696d040ac24aa414708f5c0ab': 'Aave USDC Cover',
-//   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913': 'USDC',
-//   '0xb125e6687d4313864e53df431d5425969c15eb2f': 'Compound USDC Cover',
-// };
-
-// export const TOKEN_LOGO_MAP = {
-//   '0x4e65fe4dba92790696d040ac24aa414708f5c0ab': '/images/protocols/aave.png',
-//   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913': '/images/tokens/usdc.png',
-//   '0xb125e6687d4313864e53df431d5425969c15eb2f': '/images/protocols/compound.png',
-// };
-
-// export const TOKEN_DESCRIPTION_MAP = {
-//   '0x4e65fe4dba92790696d040ac24aa414708f5c0ab': 'Decentralized lending protocol',
-//   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913': 'Decentralized lending protocol',
-//   '0xb125e6687d4313864e53df431d5425969c15eb2f': 'Decentralized lending protocol',
-// };
-
 export const PROTOCOL_NAME_MAP = {
   0: 'Aave V3',
   1: 'Compound V3',
