b17d1fdab92977f6f362799cfc270671e27a2c2c
TJ <58812386+TJ-2@users.noreply.github.com>
2025-07-16 15:00:21 +0100

    fix deployment mapping for cat pool

--- frontend/app/config/deployments.js
+++ frontend/app/config/deployments.js
@@ -11,7 +11,7 @@ try {
     underwriterManager: item.UnderwriterManager || item.RiskManager,
     protocolConfigurator: item.ProtocolConfigurator || item.RiskManager,
     capitalPool: item.CapitalPool,
-    backstopPool: item.CatInsurancePool,
+    backstopPool: item.CatInsurancePool || item.BackstopPool,
     poolRegistry: item.PoolRegistry,
     policyManager: item.PolicyManager,
     priceOracle: item.PriceOracle,
